5184472f98d23bcc5f1a3a80b5f3aec27fd64a06
Jason Bian <jasonzb@umich.edu>
2023-06-10 16:41:30 -0400

    testing utils

--- homework1/homework/utils.py
+++ homework1/homework/utils.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import os
 
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
@@ -17,28 +18,25 @@ class SuperTuxDataset(Dataset):
         """
         self.dataset_path = dataset_path
         with open(self.dataset_path + '/labels.csv', 'r') as f:
-            self.labels = f.readlines()
+            #organized labels as a list of lists and skip the header
+            self.labels = [line.split(',') for line in f.readlines()][1:]
+            
 
     def __len__(self):
         """
         Your code here
         """
-        return len(self.labels)
+        return len(self.labels) - 1
 
-    def __getitem__(self, idx):
-        """
-        Your code here
-        return a tuple: img, label
-        """
+def __getitem__(self, idx):
+        image_name = self.image_paths[idx]
+        image_path = os.path.join(self.dataset_path, image_name)
+        label = int(image_name.split('.')[0])  # Extract label from the image name
 
-        #split the label into img_path and label (img_path is local path)
-        img_path, label = self.labels[idx].split(',')
-        
-        img = Image.open(self.dataset_path + '/' + img_path)
-        img = transforms.ToTensor()(img)
-        label = int(label)
-        return img, label
+        image = Image.open(image_path)
+        # You can perform any necessary transformations on the image here (e.g., resizing, normalization)
 
+        return image, label
 
 def load_data(dataset_path, num_workers=0, batch_size=128):
     dataset = SuperTuxDataset(dataset_path)
@@ -48,3 +46,16 @@ def load_data(dataset_path, num_workers=0, batch_size=128):
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
+
+'''
+#run the SuperTuxDataset(Dataset): class
+if __name__ == '__main__':
+    test = SuperTuxDataset('homework1/data/train')
+    #Run getitem
+    test.__getitem__(0)
+    #Run len
+    test.__len__()
+'''
+
+
+
