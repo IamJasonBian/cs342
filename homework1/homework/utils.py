588a59a605dc8fa086c007dbf57e9a1819391618
Jason Bian <jasonzb@umich.edu>
2023-06-10 16:56:50 -0400

    loader working

--- homework1/homework/utils.py
+++ homework1/homework/utils.py
@@ -1,42 +1,44 @@
 from PIL import Image
-
+import csv
+import os
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-import os
-
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
-
 class SuperTuxDataset(Dataset):
     def __init__(self, dataset_path):
-        """
-        Your code here
-        Hint: Use the python csv library to parse labels.csv
-
-        WARNING: Do not perform data normalization here. 
-        """
         self.dataset_path = dataset_path
-        with open(self.dataset_path + '/labels.csv', 'r') as f:
-            #organized labels as a list of lists and skip the header
-            self.labels = [line.split(',') for line in f.readlines()][1:]
+        self.image_paths = []
+        self.labels = []
+        
+        # Read labels from the CSV file
+        with open(os.path.join(dataset_path, 'labels.csv'), 'r') as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)  # Skip the header
+            for row in csv_reader:
+                image_path = os.path.join(dataset_path, row[0])
+
+                # Convert label string to integer by matching to the self.labels object
+                label = LABEL_NAMES.index(row[1])
+                self.image_paths.append(image_path)
+                self.labels.append(label)
+        
+        self.transform = transforms.Compose([
+            transforms.ToTensor()
             
+            #,transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
     def __len__(self):
-        """
-        Your code here
-        """
-        return len(self.labels) - 1
+        return len(self.image_paths)
 
-def __getitem__(self, idx):
-        image_name = self.image_paths[idx]
-        image_path = os.path.join(self.dataset_path, image_name)
-        label = int(image_name.split('.')[0])  # Extract label from the image name
-
-        image = Image.open(image_path)
-        # You can perform any necessary transformations on the image here (e.g., resizing, normalization)
-
+    def __getitem__(self, idx):
+        image = Image.open(self.image_paths[idx]).convert('RGB')
+        image = self.transform(image)
+        label = self.labels[idx]
         return image, label
+
 
 def load_data(dataset_path, num_workers=0, batch_size=128):
     dataset = SuperTuxDataset(dataset_path)
@@ -47,7 +49,7 @@ def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
 
-'''
+
 #run the SuperTuxDataset(Dataset): class
 if __name__ == '__main__':
     test = SuperTuxDataset('homework1/data/train')
@@ -55,7 +57,7 @@ if __name__ == '__main__':
     test.__getitem__(0)
     #Run len
     test.__len__()
-'''
+
 
 
 
