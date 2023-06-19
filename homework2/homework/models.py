8e4df2d5078426a337de398a4ab1796d23c75bbc
Jason Bian <jasonzb@umich.edu>
2023-06-19 13:58:12 -0700

    working train

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -4,6 +4,22 @@ import torch.nn.init as init
 import torch.nn.functional as F
 
 
+class LossType(str):
+    """Standard names for loss type
+    """
+    SOFTMAX_CROSS_ENTROPY = "SoftmaxCrossEntropy"
+    SOFTMAX_FOCAL_CROSS_ENTROPY = "SoftmaxFocalCrossEntropy"
+    SIGMOID_FOCAL_CROSS_ENTROPY = "SigmoidFocalCrossEntropy"
+    BCE_WITH_LOGITS = "BCEWithLogitsLoss"
+
+    @classmethod
+    def str(cls):
+        return ",".join([cls.SOFTMAX_CROSS_ENTROPY,
+                         cls.SOFTMAX_FOCAL_CROSS_ENTROPY,
+                         cls.SIGMOID_FOCAL_CROSS_ENTROPY,
+                         cls.BCE_WITH_LOGITS])
+
+
 class CNNClassifier(torch.nn.Module):
     def __init__(self):
         """
@@ -18,7 +34,7 @@ class CNNClassifier(torch.nn.Module):
         print("mid net")
 
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(901*3, 120)  # 5*5 from image dimension 
+        self.fc1 = nn.Linear(2704, 120)  # 5*5 from image dimension 
         
         print("late net")
 
@@ -42,7 +58,63 @@ class CNNClassifier(torch.nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+class ActivationType(str):
+    """Standard names for activation type
+    """
+    SOFTMAX = "Softmax"
+    SIGMOID = "Sigmoid"
 
+    @classmethod
+    def str(cls):
+        return ",".join([cls.SOFTMAX,
+                         cls.SIGMOID])
+
+    
+class ClassificationLoss(torch.nn.Module):
+    def __init__(self, class_weight=None,
+                 loss_type=LossType.SOFTMAX_CROSS_ENTROPY):
+        super(ClassificationLoss, self).__init__()
+
+        self.loss_type = loss_type
+        if loss_type == LossType.SOFTMAX_CROSS_ENTROPY:
+            self.criterion = torch.nn.CrossEntropyLoss(class_weight)
+        elif loss_type == LossType.BCE_WITH_LOGITS:
+            self.criterion = torch.nn.BCEWithLogitsLoss()
+        else:
+            raise TypeError(
+                "Unsupported loss type: %s. Supported loss type is: %s" % (
+                    loss_type, LossType.str()))
+
+    def forward(self, logits, target,
+                use_hierar=False,
+                is_multi=False,
+                *argvs):
+        device = logits.device
+        if use_hierar:
+            assert self.loss_type in [LossType.BCE_WITH_LOGITS,
+                                      LossType.SIGMOID_FOCAL_CROSS_ENTROPY]
+            if not is_multi:
+                target = torch.eye(self.label_size)[target].to(device)
+            hierar_penalty, hierar_paras, hierar_relations = argvs[0:3]
+            return self.criterion(logits, target) + \
+                   hierar_penalty * self.cal_recursive_regularize(hierar_paras,
+                                                                  hierar_relations,
+                                                                  device)
+        else:
+            if is_multi:
+                assert self.loss_type in [LossType.BCE_WITH_LOGITS,
+                                          LossType.SIGMOID_FOCAL_CROSS_ENTROPY]
+            else:
+                if self.loss_type not in [LossType.SOFTMAX_CROSS_ENTROPY,
+                                          LossType.SOFTMAX_FOCAL_CROSS_ENTROPY]:
+                    target = torch.eye(self.label_size)[target].to(device)
+            return self.criterion(logits, target)
+
+
+model_factory = {
+    'cnn': CNNClassifier,
+}
 
 def save_model(model):
     from torch import save
--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -1,31 +1,60 @@
-from .models import CNNClassifier, save_model
+from .models import ClassificationLoss, model_factory, save_model
 from .utils import accuracy, load_data
 import torch
-import torch.utils.tensorboard as tb
-
 
 def train(args):
-    from os import path
-    model = CNNClassifier()
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
+    model = model_factory[args.model]()
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    """
-    Your code here, modify your HW1 code
-    
-    """
+    model.to(device)
+    if args.continue_training:
+        from os import path
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % args.model)))
 
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9)
+    criterion = ClassificationLoss()
+
+    train_data = load_data('data/train')
+    valid_data = load_data('data/valid')
+
+    for epoch in range(args.num_epoch):
+        model.train()
+        loss_vals, acc_vals, vacc_vals = [], [], []
+        for img, label in train_data:
+            img, label = img.to(device), label.to(device)
+
+            logit = model(img)
+            loss_val = criterion(logit, label)
+            acc_val = accuracy(logit, label)
+
+            loss_vals.append(loss_val.detach().cpu().numpy())
+            acc_vals.append(acc_val.detach().cpu().numpy())
+
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+
+        avg_loss = sum(loss_vals) / len(loss_vals)
+        avg_acc = sum(acc_vals) / len(acc_vals)
+
+        model.eval()
+        for img, label in valid_data:
+            img, label = img.to(device), label.to(device)
+            vacc_vals.append(accuracy(model(img), label).detach().cpu().numpy())
+        avg_vacc = sum(vacc_vals) / len(vacc_vals)
+
+        print('epoch %-3d \t loss = %0.3f \t acc = %0.3f \t val acc = %0.3f' % (epoch, avg_loss, avg_acc, avg_vacc))
     save_model(model)
-
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir')
-    # Put custom arguments here
+
+    parser.add_argument('-m', '--model', choices=['cnn'], default='linear')
+    parser.add_argument('-n', '--num_epoch', type=int, default=50)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-c', '--continue_training', action='store_true')
 
     args = parser.parse_args()
     train(args)
