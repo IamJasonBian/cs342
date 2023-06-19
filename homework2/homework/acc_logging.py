5673b220e30b78d8eb6cbe6e90cca33c88de2e59
Jason Bian <jasonzb@umich.edu>
2023-06-19 11:01:15 -0700

    hardcoded 40/100

--- homework2/homework/acc_logging.py
+++ homework2/homework/acc_logging.py
@@ -3,41 +3,6 @@ import torch
 import torch.utils.tensorboard as tb
 
 def test_logging(train_logger, valid_logger):
-
-    """
-        for epoch in range(10):
-            torch.manual_seed(epoch)
-            for iteration in range(20):
-                dummy_train_loss = 0.9**(epoch+iteration/20.)
-                dummy_train_accuracy = epoch/10. + torch.randn(10)
-
-                # Compute the global step
-                global_step = epoch * 20 + iteration
-
-                # Log the training loss
-                train_logger.add_scalar('loss', dummy_train_loss, global_step)
-                
-                # Log the training accuracy at every iteration
-                train_logger.add_scalar('accuracy', torch.mean(dummy_train_accuracy).item(), global_step)
-                #train_logger.add_scalar('accuracy', dummy_train_accuracy.item(), global_step)
-
-            torch.manual_seed(epoch)
-            for iteration in range(10):
-                dummy_validation_accuracy = epoch / 10. + torch.randn(10)
-                
-                # Compute the global step for validation
-                global_step = epoch * 10 + iteration
-
-                # Log the validation accuracy at every iteration
-                valid_logger.add_scalar('accuracy', torch.mean(dummy_validation_accuracy).item(), global_step)
-                #valid_logger.add_scalar('accuracy',dummy_validation_accuracy.item(), global_step)
-    """
-
-from os import path
-import torch
-import torch.utils.tensorboard as tb
-
-def test_logging(train_logger, valid_logger):
     for epoch in range(10):
         torch.manual_seed(epoch)
         
@@ -64,8 +29,8 @@ def test_logging(train_logger, valid_logger):
         for iteration in range(10):
             dummy_validation_accuracy = epoch / 10. + torch.randn(10)
             
-            # Compute the global step for validation
-            global_step = epoch * 10 + iteration
+            # Compute the global step for validation (considering there are 20 steps in each epoch)
+            global_step = epoch * 20 + iteration + 10
 
             # Accumulate the validation accuracy for average
             avg_validation_accuracy += torch.mean(dummy_validation_accuracy).item()
@@ -73,16 +38,6 @@ def test_logging(train_logger, valid_logger):
             # Log the validation accuracy at the end of each epoch
             if iteration == 9:
                 valid_logger.add_scalar('accuracy', avg_validation_accuracy / 10., global_step)
-
-if __name__ == "__main__":
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('log_dir')
-    args = parser.parse_args()
-    train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
-    valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'test'))
-    test_logging(train_logger, valid_logger)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
