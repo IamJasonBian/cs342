204182eb5caf81759f4f4fd8f46c7e21194b89c5
Jason Bian <jasonzb@umich.edu>
2023-08-01 17:13:47 -0700

    final project

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -16,6 +16,48 @@ def train(args):
     Your code here, modify your HW4 code
     Hint: Use the log function below to debug and visualize your model
     """
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-6)
+
+    
+    train_data = load_detection_data('dense_data/train', num_workers=4)
+    valid_data = load_detection_data('dense_data/valid', num_workers=4)
+
+    global_step = 0
+    for epoch in range(args.num_epoch):
+        model.train()
+        for img, label,_ in train_data:
+            img, label = img.to(device), label.to(device)
+
+            logit = model(img)
+            loss_train = torch.mean(loss(logit, label))
+            
+            if train_logger is not None and global_step % 100 == 0:
+                log(train_logger, img, label, logit, global_step)
+
+            if train_logger is not None:
+                train_logger.add_scalar('loss', loss_train, global_step)
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+            global_step += 1
+
+        model.eval()
+        
+        for img, label,_ in valid_data:
+            img, label = img.to(device), label.to(device)
+            logit = model(img)
+            loss_valid = torch.mean(loss(logit, label))
+
+
+        if valid_logger is not None:
+            log(valid_logger, img, label, logit, global_step)
+
+
+        if valid_logger is None or train_logger is None:
+            print('epoch %-3d  \t train loss = %0.3f \t val loss = %0.3f' %
+                  (epoch,loss_train,loss_valid))
+        save_model(model)
+
 
     save_model(model)
 
